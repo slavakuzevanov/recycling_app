8dae6c205d7dbf00f3c1ce90bbabfa751321ee98
Vyacheslav Kuzevanov <Slava.kuzevanov@mail.ru>
2024-05-18 16:05:41 +0300

    added user location listener

--- RecyclingApp/MapViewController.swift
+++ RecyclingApp/MapViewController.swift
@@ -2,82 +2,93 @@
 //  MapViewController.swift
 //  RecyclingApp
 //
-//  Created by Вячеслав Кузеванов on 12.05.2024.
+//  Created by Вячеслав Кузеванов on 18.05.2024.
 //
 
-import Foundation
 import UIKit
 import YandexMapsMobile
+import CoreLocation
 
-/**
- * This example shows how to display and customize user location arrow on the map.
- */
-class MapViewController: BaseMapViewController, YMKUserLocationObjectListener {
+class MapViewController: UIViewController, YMKUserLocationObjectListener, CLLocationManagerDelegate {
     
+    @IBOutlet weak var mapView: YMKMapView!
+    // Подключаем IBOutlet для YMKMapView
+
+    var userLocationLayer: YMKUserLocationLayer!
+    var locationManager: CLLocationManager!
+    var isFirstLocationUpdate = true
+
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        mapView.mapWindow.map.isRotateGesturesEnabled = false
-        mapView.mapWindow.map.move(with:
-            YMKCameraPosition(target: YMKPoint(latitude: 0, longitude: 0), zoom: 14, azimuth: 0, tilt: 0))
-        
-        let scale = UIScreen.main.scale
-        let mapKit = YMKMapKit.sharedInstance()
-        let userLocationLayer = mapKit.createUserLocationLayer(with: mapView.mapWindow)
-
-        userLocationLayer.setVisibleWithOn(true)
+        // Инициализация слоя для отображения местоположения пользователя
+        userLocationLayer = YMKMapKit.sharedInstance().createUserLocationLayer(with: mapView.mapWindow)
+        userLocationLayer.setVisibleWithOn(true) // Исправлено на вызов метода
         userLocationLayer.isHeadingEnabled = true
-        userLocationLayer.setAnchorWithAnchorNormal(
-            CGPoint(x: 0.5 * mapView.frame.size.width * scale, y: 0.5 * mapView.frame.size.height * scale),
-            anchorCourse: CGPoint(x: 0.5 * mapView.frame.size.width * scale, y: 0.83 * mapView.frame.size.height * scale))
         userLocationLayer.setObjectListenerWith(self)
-        
-        
-        
-    }
-    
-    func onObjectAdded(with view: YMKUserLocationView) {
-        view.arrow.setIconWith(UIImage(named:"UserArrow")!)
-        
-        let pinPlacemark = view.pin.useCompositeIcon()
-        
-//        pinPlacemark.setIconWithName("Icon",
-//            image: UIImage(named:"Icon")!,
-//            style:YMKIconStyle(
-//                anchor: CGPoint(x: 0, y: 0) as NSValue,
-//                rotationType:YMKRotationType.rotate.rawValue as NSNumber,
-//                zIndex: 0,
-//                flat: true,
-//                visible: true,
-//                scale: 1.5,
-//                tappableArea: nil))
-        
-        pinPlacemark.setIconWithName(
-            "pin",
-            image: UIImage(named:"SearchResult")!,
-            style:YMKIconStyle(
-                anchor: CGPoint(x: 0.5, y: 0.5) as NSValue,
-                rotationType:YMKRotationType.rotate.rawValue as NSNumber,
-                zIndex: 1,
-                flat: true,
-                visible: true,
-                scale: 1,
-                tappableArea: nil))
 
+        // Настройка и отображение карты
+//        mapView.mapWindow.map.move(
+//            with: YMKCameraPosition(target: YMKPoint(latitude: 55.751244, longitude: 37.618423), zoom: 14, azimuth: 0, tilt: 0)
+//        )
+
+        // Настройка CLLocationManager для запроса разрешения на использование геолокации
+        locationManager = CLLocationManager()
+        locationManager.delegate = self
+        locationManager.requestWhenInUseAuthorization() // Запрос разрешения на использование геолокации
+    }
+
+    // CLLocationManagerDelegate методы
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        switch status {
+        case .notDetermined:
+            manager.requestWhenInUseAuthorization()
+        case .restricted, .denied:
+            print("Location access denied or restricted")
+        case .authorizedWhenInUse, .authorizedAlways:
+            manager.startUpdatingLocation()
+        @unknown default:
+            break
+        }
+    }
+
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        if let location = locations.last {
+            let latitude = location.coordinate.latitude
+            let longitude = location.coordinate.longitude
+            print("Latitude: \(latitude), Longitude: \(longitude)")
+        }
+    }
+
+    // YMKUserLocationObjectListener методы
+    func onObjectAdded(with view: YMKUserLocationView) {
+        view.pin.setIconWith(UIImage(named: "SearchResult")!)
+        view.arrow.setIconWith(UIImage(named: "UserArrow")!)
         view.accuracyCircle.fillColor = UIColor(
                                                 red: 30.0 / 255,
                                                 green: 70.0 / 255,
                                                 blue: 200.0 / 255,
                                                 alpha: 0.3
-                                               )
-        print(view.pin.geometry.latitude, view.pin.geometry.longitude)
-        
+                                                )
     }
 
-    func onObjectRemoved(with view: YMKUserLocationView) {}
+    func onObjectRemoved(with view: YMKUserLocationView) {
+        // Обработка удаления объекта местоположения пользователя (если необходимо)
+    }
 
     func onObjectUpdated(with view: YMKUserLocationView, event: YMKObjectEvent) {
-        print("Обновление user location")
-        print(view.pin.geometry.latitude, view.pin.geometry.longitude)
-    }
+            guard isFirstLocationUpdate else {
+                return
+            }
+            isFirstLocationUpdate = false
+            
+            if let location = userLocationLayer.cameraPosition()?.target {
+                // Плавный зум к местоположению пользователя
+                mapView.mapWindow.map.move(
+                    with: YMKCameraPosition(target: location, zoom: 14, azimuth: 0, tilt: 0),
+                    animation: YMKAnimation(type: .smooth, duration: 1.5),
+                    cameraCallback: nil
+                )
+            }
+        }
 }
