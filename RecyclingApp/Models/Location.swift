a09db75848f2d77ff9f5df7c5c119e2532fb63c6
Vyacheslav Kuzevanov <Slava.kuzevanov@mail.ru>
2024-05-20 19:48:17 +0300

    added location preview

--- RecyclingApp/Models/Location.swift
+++ RecyclingApp/Models/Location.swift
@@ -8,7 +8,8 @@
 import Foundation
 import MapKit
 
-struct Location: Identifiable {
+struct Location: Identifiable, Equatable {
+    
     let name: String
     let cityName: String
     let coordinates: CLLocationCoordinate2D
@@ -19,6 +20,11 @@ struct Location: Identifiable {
     // Identifiable
     var id: String {
         name + cityName
+    }
+    
+    // Equatable
+    static func == (lhs: Location, rhs: Location) -> Bool {
+        lhs.id == rhs.id
     }
 }
 
--- RecyclingApp/Views/LocationsView.swift
+++ RecyclingApp/Views/LocationsView.swift
@@ -25,32 +25,45 @@ struct LocationsView: View {
             
             VStack(spacing: 0) {
                 header
+                    .frame(maxWidth: .infinity, maxHeight: 700, alignment: .topLeading)
                     .padding(10)
+                    //.background(Color.orange)
+                
+                // Right side buttons
+                HStack{
+                    Spacer()
+                    VStack(alignment: .leading) {
+                        Spacer()
+                        userLocationButton
+                        Spacer()
+                        mapRegionButton
+                    }
+                    .padding(8)
+                    //.background(Color.black)
+                    .frame(maxWidth: .infinity, alignment: .trailing) // Выровнять по правому краю
+                }
+                .frame(maxWidth: .infinity, maxHeight: 200, alignment: .topLeading)
+                //.background(Color.blue)
+                
+                
                 Spacer()
                 
-                Button(action: vm.scrollToUserLocation) {
-                    Image(systemName: "safari.fill")
-                        .foregroundColor(Color(red: 239 / 255, green: 177 / 255, blue: 154 / 255))
+                ZStack {
+                    ForEach(vm.locations) {location in
+                        if vm.mapLocation == location {
+                            LocationPreviewView(location: location)
+                                .shadow(color: .black.opacity(0.3), radius: 5)
+                                .padding()
+                                .offset(y: -70)
+                                .transition(.asymmetric(
+                                    insertion: .move(edge: .trailing),
+                                    removal: .move(edge: .leading)))
+                        }
+                    }
                 }
-                .frame(width: Layout.buttonSize, height: Layout.buttonSize)
-                .background(Color(red: 154 / 255, green: 181 / 255, blue: 107 / 255))
-                .cornerRadius(10)
-                .padding(.trailing, Layout.buttonMargin)
-                .padding(.bottom, Layout.buttonBottomOffset) // Используем новый отступ снизу
-                .position(x: UIScreen.main.bounds.width - Layout.buttonSize / 2 - Layout.buttonMargin,
-                          y: UIScreen.main.bounds.height - Layout.buttonSize / 2 - Layout.buttonBottomOffset)
+                //.background(Color.yellow)
                 
-                Button(action: vm.scrollToCurrentRegion) {
-                    Image(systemName: "safari.fill")
-                        .foregroundColor(Color(red: 239 / 255, green: 177 / 255, blue: 154 / 255))
-                }
-                .frame(width: Layout.buttonSize, height: Layout.buttonSize)
-                .background(Color(red: 154 / 255, green: 0 / 255, blue: 107 / 255))
-                .cornerRadius(10)
-                .padding(.trailing, Layout.buttonMargin)
-                .padding(.bottom, Layout.buttonBottomOffset + 1000) // Используем новый отступ снизу
-                .position(x: UIScreen.main.bounds.width - Layout.buttonSize / 2 - Layout.buttonMargin,
-                          y: UIScreen.main.bounds.height - Layout.buttonSize / 2 - Layout.buttonBottomOffset)
+                
                 
             }
         }
@@ -67,6 +80,7 @@ extension LocationsView {
                     .foregroundColor(.primary)
                     .frame(height: 55)
                     .frame(maxWidth: .infinity)
+                    .animation(.none, value: vm.mapLocation)
                     .overlay(alignment: .leading) {
                         Image(systemName: "arrow.down")
                             .font(.headline)
@@ -84,6 +98,35 @@ extension LocationsView {
         .background(.thickMaterial)
         .clipShape(RoundedRectangle(cornerRadius: 10))
         .shadow(color: Color.black.opacity(0.3), radius: 20, x: 0, y: 15)
+    }
+    
+    private var userLocationButton: some View {
+        Button(action: {
+            vm.scrollToUserLocation()
+            print("Button tapped \(vm.userLocation?.latitude) \(vm.userLocation?.longitude)")
+            print("Button tapped current location \(vm.mapRegion.latitude) \(vm.mapRegion.longitude)")
+        }, label: {
+            Image(systemName: "safari.fill")
+                .foregroundColor(Color(red: 239 / 255, green: 177 / 255, blue: 154 / 255))
+        })
+        .frame(width: Layout.buttonSize, height: Layout.buttonSize)
+        .background(Color(red: 154 / 255, green: 181 / 255, blue: 107 / 255))
+        .cornerRadius(10)
+        .padding(.leading, Layout.buttonMargin)
+//        .padding(.bottom, Layout.buttonBottomOffset) // Используем новый отступ снизу
+
+    }
+    
+    private var mapRegionButton: some View{
+        Button(action: vm.scrollToCurrentRegion) {
+            Image(systemName: "safari.fill")
+                .foregroundColor(Color(red: 239 / 255, green: 177 / 255, blue: 154 / 255))
+        }
+        .frame(width: Layout.buttonSize, height: Layout.buttonSize)
+        .background(Color(red: 154 / 255, green: 0 / 255, blue: 107 / 255))
+        .cornerRadius(10)
+        .padding(.leading, Layout.buttonMargin)
+//        .padding(.bottom, Layout.buttonBottomOffset) // Используем новый отступ снизу
     }
 }
 
